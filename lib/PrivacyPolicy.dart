c90ba462ccf61e7586e7305d37db793e7f60b6ff
Mohammad Almasri <mohammadmasri349@gmail.com>
2024-12-22 19:26:14 +0100

    adding the privacy and policy page

--- lib/PrivacyPolicy.dart
+++ lib/PrivacyPolicy.dart
@@ -1,135 +1,54 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: PrivacyPolicyPage(),
-    );
-  }
-}
-
-class PrivacyPolicyPage extends StatelessWidget {
+class PrivacyPolicy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Privacy Policy'),
+        title: Text("Privacy Policy"),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context); // This will handle the back button
+            Navigator.pop(context);
           },
         ),
       ),
       body: SingleChildScrollView(
-        child: Stack(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Background color for the page
-            Container(
-              color: Colors.white,
-              width: double.infinity,
-              height: 1000, // This is for scrolling, you can adjust this
+            Text(
+              "1. Types of Data We Collect",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
-            // Privacy Policy title
-            Positioned(
-              left: 87,
-              top: 65,
-              child: Text(
-                'Privacy Policy',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  height: 1.27, // line height equivalent to 28px
-                ),
-              ),
+            SizedBox(height: 8.0),
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n\n"
+              "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+              style: TextStyle(fontSize: 16.0),
             ),
-            // First section - Types of data we collect
-            Positioned(
-              left: 24,
-              top: 125,
-              child: Text(
-                '1. Types of data we collect',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  letterSpacing: 0.1,
-                ),
-              ),
+            SizedBox(height: 16.0),
+            Text(
+              "2. Use of Your Personal Data",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
-            Positioned(
-              left: 24,
-              top: 155,
-              right: 24,
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  fontSize: 14,
-                  color: Colors.black,
-                  height: 1.14, // line height equivalent to 16px
-                  letterSpacing: 0.25,
-                ),
-              ),
+            SizedBox(height: 8.0),
+            Text(
+              "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae.\n\n"
+              "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit.",
+              style: TextStyle(fontSize: 16.0),
             ),
-            // Second section - Use of your personal data
-            Positioned(
-              left: 24,
-              top: 341,
-              child: Text(
-                '2. Use of your personal data',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  letterSpacing: 0.1,
-                ),
-              ),
+            SizedBox(height: 16.0),
+            Text(
+              "3. Disclosure of Your Personal Data",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
             ),
-            // Third section - Disclosure of your personal data
-            Positioned(
-              left: 24,
-              top: 517,
-              child: Text(
-                '3. Disclosure of your personal data',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  letterSpacing: 0.1,
-                ),
-              ),
-            ),
-            // Arrow (Vector) Icon on the left side
-            Positioned(
-              left: 24,
-              top: 69,
-              child: Icon(
-                Icons.arrow_left,
-                size: 24,
-                color: Colors.black,
-              ),
-            ),
-            // Background section (Vector with background color)
-            Positioned(
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0,
-              child: Container(
-                color: Colors.black,
-                margin: EdgeInsets.symmetric(horizontal: 24, vertical: 69),
-              ),
+            SizedBox(height: 8.0),
+            Text(
+              "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.\n\n"
+              "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.",
+              style: TextStyle(fontSize: 16.0),
             ),
           ],
         ),
