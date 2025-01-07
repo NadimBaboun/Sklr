65d26b1fd5fe8bbafe4ca58b67dd795e7d52189c
ai28 <inr23001@student.mdu.se>
2025-01-07 20:16:46 +0100

    /api/sessions
    POST, GET {session_id}, PATCH {session_id}, DELETE {session_id}
    TODO: GET by user_id for both requester & provider

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,11 +6,13 @@ const testRoutes = require("./test");
 const authRoutes = require("./auth");
 const userRoutes = require("./users");
 const chatRoutes = require("./chat");
+const sessionRoutes = require("./sessions");
 
 // make use of imported routes
 router.use('/api', testRoutes);
 router.use('/api', authRoutes);
 router.use('/api/users', userRoutes);
 router.use('/api/chat', chatRoutes);
+router.use('/api/sessions', sessionRoutes);
 
 module.exports = router;
