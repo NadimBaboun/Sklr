ebb473dad6d08cdcf642b848c74ed740a55078fd
Wilma Wengborn <wwn22001@student.mdu.se>
2025-01-07 20:11:17 +0100

    Fetch skills from database

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,11 +6,13 @@ const testRoutes = require("./test");
 const authRoutes = require("./auth");
 const userRoutes = require("./users");
 const chatRoutes = require("./chat");
+const skillsRoutes = require("./skills");
 
 // make use of imported routes
 router.use('/api', testRoutes);
 router.use('/api', authRoutes);
 router.use('/api/users', userRoutes);
 router.use('/api/chat', chatRoutes);
+router.use('/api/skills', skillsRoutes);
 
 module.exports = router;
