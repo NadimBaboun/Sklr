5c2bf81baceff380fc0d8d75b347d414db466754
Mohammad Hamarsheh <73851911+hamarshehmhmd@users.noreply.github.com>
2025-02-21 18:46:14 +0200

    UI Fix + Functionality
    Added the possibility to uploaad photo on profile picture, fixed the ui of the profile.dart page

--- lib/Profile/profile.dart
+++ lib/Profile/profile.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:sklr/Util/PrivacyPolicy.dart';
 import 'package:sklr/Profile/dashboard.dart';
 import 'package:sklr/database/userIdStorage.dart';
 import 'package:sklr/Util/startpage.dart';
 import 'dart:io';
-import 'editProfile.dart'; 
+import 'editProfile.dart';
 import '../Util/navigationbar-bar.dart';
 import '../database/database.dart';
 
@@ -20,10 +21,11 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   File? _image;
   final ImagePicker _picker = ImagePicker();
-
+  final supabase = Supabase.instance.client;
   Map<String, dynamic>? userData;
   bool isLoading = true;
   bool isModerator = false;
+  String? _avatarUrl;
 
   @override
   void initState() {
@@ -33,243 +35,268 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Future<void> _loadUserData() async {
     final userId = await UserIdStorage.getLoggedInUserId();
-
     if (userId != null && userId > 0) {
       final response = await DatabaseHelper.fetchUserFromId(userId);
       if (response.success) {
         setState(() {
           userData = response.data;
+          _avatarUrl = userData?['avatar_url'];
           isLoading = false;
           isModerator = userData!['moderator'];
-        });
-      } else {
-        setState(() {
-          isLoading = true;
         });
       }
     }
   }
-  //Didnt have time to implement storing images into database
-/*
-  Future<void> _pickImage(ImageSource source) async {
-    final pickedFile = await _picker.pickImage(source: source);
-    if (pickedFile != null) {
+
+  Future<void> _pickAndUploadImage() async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: ImageSource.gallery,
+        maxWidth: 300,
+        maxHeight: 300,
+      );
+
+      if (pickedFile == null) return;
+
+      setState(() => isLoading = true);
+
+      final userId = await UserIdStorage.getLoggedInUserId();
+      final bytes = await pickedFile.readAsBytes();
+      final fileExt = pickedFile.path.split('.').last;
+      final fileName =
+          '$userId-${DateTime.now().millisecondsSinceEpoch}.$fileExt';
+      final filePath = 'profiles/$userId/$fileName';
+
+      await supabase.storage.from('profile-pictures').uploadBinary(
+            filePath,
+            bytes,
+            fileOptions:
+                const FileOptions(contentType: 'image/jpeg', upsert: true),
+          );
+
+      final imageUrl =
+          supabase.storage.from('profile-pictures').getPublicUrl(filePath);
+
+      final response = await supabase
+          .from('users')
+          .update({'avatar_url': imageUrl}).eq('id', userId as Object);
+
+      if (response.error != null) {
+        throw Exception(response.error!.message);
+      }
+
       setState(() {
-        _image = File(pickedFile.path);
+        _avatarUrl = imageUrl;
+        userData?['avatar_url'] = imageUrl;
+        isLoading = false;
       });
+
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text('Profile picture updated successfully!')),
+        );
+      }
+    } catch (error) {
+      setState(() => isLoading = false);
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error uploading image: $error')),
+        );
+      }
     }
   }
 
-  void _showImagePickerOptions() {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return SafeArea(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                leading: Icon(Icons.photo_library),
-                title: Text('Choose from Gallery'),
-                onTap: () {
-                  Navigator.pop(context);
-                  _pickImage(ImageSource.gallery);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.camera_alt),
-                title: Text('Take a Picture'),
-                onTap: () {
-                  Navigator.pop(context);
-                  _pickImage(ImageSource.camera);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.delete),
-                title: Text('Remove Picture'),
-                onTap: () {
-                  Navigator.pop(context);
-                  setState(() {
-                    _image = null;
-                  });
-                },
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 40),
-            Container(
-              margin: const EdgeInsets.only(bottom: 20),
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Icon(
-                    Icons.account_balance_wallet,
-                    color: const Color(0xFF6296FF),
-                    size: 30,
-                  ),
-                  const SizedBox(width: 8),
-                  Text(
-                    "${userData?['credits'] ?? 0}",
-                    style: GoogleFonts.lexend(
-                      textStyle: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF6296FF),
-                      ),
-                    ),
-                  ),
+                  const SizedBox(height: 40),
+                  _buildCreditsSection(),
+                  _buildProfileImage(),
+                  const SizedBox(height: 20),
+                  _buildUserInfo(),
+                  const SizedBox(height: 20),
+                  Expanded(child: _buildOptions()),
                 ],
               ),
             ),
-            // Avatar Section
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Stack(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFDCEBFF), 
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        // Avatar image
-                        child: ClipOval(
-                          child: _image != null
-                              ? Image.file(
-                                  _image!,
-                                  fit: BoxFit.cover,
-                                  width: 80,
-                                  height: 80,
-                                )
-                              : Image.asset(
-                                  'assets/images/avatar.png',
-                                  fit: BoxFit.cover,
-                                  width: 80,
-                                  height: 80,
-                                ),
-                        ),
-                      ),
-                    ),/*
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: GestureDetector(
-                        onTap: _showImagePickerOptions,
-                        child: Container(
-                          width: 30,
-                          height: 30,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFF6296FF),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Icon(
-                            Icons.edit,
-                            color: Colors.white,
-                            size: 20,
-                          ),
-                        ),
-                      ),
-                    ),*/
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            // User Name and Details
-            Text(
-              userData?['username'] ?? 'Unknown User',
-              style: GoogleFonts.mulish(
-                textStyle: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+      bottomNavigationBar: const CustomBottomNavigationBar(currentIndex: 3),
+    );
+  }
+
+  Widget _buildCreditsSection() {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.account_balance_wallet,
+              color: Color(0xFF6296FF), size: 30),
+          const SizedBox(width: 8),
+          Text(
+            "${userData?['credits'] ?? 0}",
+            style: GoogleFonts.lexend(
+              textStyle: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF6296FF),
               ),
             ),
-            const SizedBox(height: 5),
-            Text(
-              '${userData?['email'] ?? 'No Email'} | ${userData?['phone_number'] ?? 'No Phone'}',
-              style: GoogleFonts.mulish(
-                textStyle: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey,
-                ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProfileImage() {
+    return Stack(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: const BoxDecoration(
+            color: Color(0xFFDCEBFF),
+            shape: BoxShape.circle,
+          ),
+          child: ClipOval(
+            child: _avatarUrl != null
+                ? Image.network(
+                    _avatarUrl!,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) =>
+                        Image.asset('assets/images/avatar.png'),
+                  )
+                : Image.asset('assets/images/avatar.png'),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: GestureDetector(
+            onTap: _pickAndUploadImage,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: const BoxDecoration(
+                color: Color(0xFF6296FF),
+                shape: BoxShape.circle,
               ),
-              textAlign: TextAlign.center,
+              child:
+                  const Icon(Icons.camera_alt, color: Colors.white, size: 20),
             ),
-            const SizedBox(height: 20),
-            // Profile Settings
-            Expanded(
-              child: ListView(
-                children: [
-                  isModerator
-                      ? OptionTile(
-                          icon: Icons.report_gmailerrorred_outlined,
-                          title: 'Reports',
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ModeratorDashboard()),
-                            );
-                          })
-                      : SizedBox.shrink(),
-                  const Divider(height: 20),
-                  OptionTile(
-                    icon: Icons.person_outline,
-                    title: 'Edit profile information',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => EditProfilePage()),
-                      );
-                    },
-                  ),
-                  OptionTile(
-                    icon: Icons.privacy_tip_outlined,
-                    title: 'Privacy policy',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => PrivacyPolicy()),
-                      );
-                    },
-                  ),
-                  OptionTile(
-                    icon: Icons.logout_outlined,
-                    title: 'Sign Out',
-                    onTap: () {
-                      showSignOutDialog(context);
-                    },
-                  ),
-                ],
-              ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildUserInfo() {
+    return Column(
+      children: [
+        Text(
+          userData?['username'] ?? 'Unknown User',
+          style: GoogleFonts.mulish(
+            textStyle: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+        Text(
+          '${userData?['email'] ?? 'No Email'} | ${userData?['phone_number'] ?? 'No Phone'}',
+          style: GoogleFonts.mulish(
+            textStyle: const TextStyle(
+              fontSize: 14,
+              color: Colors.grey,
+            ),
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildOptions() {
+    return ListView(
+      children: [
+        if (isModerator) ...[
+          OptionTile(
+            icon: Icons.report_gmailerrorred_outlined,
+            title: 'Reports',
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ModeratorDashboard()),
+            ),
+          ),
+          const Divider(height: 20),
+        ],
+        OptionTile(
+          icon: Icons.person_outline,
+          title: 'Edit profile information',
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => EditProfilePage()),
+          ),
+        ),
+        OptionTile(
+          icon: Icons.privacy_tip_outlined,
+          title: 'Privacy policy',
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => PrivacyPolicy()),
+          ),
+        ),
+        OptionTile(
+          icon: Icons.logout_outlined,
+          title: 'Sign Out',
+          onTap: () => _showSignOutDialog(context),
+        ),
+      ],
+    );
+  }
+
+  void _showSignOutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Confirm sign out?"),
+          content: const Text(
+              "If you sign out, you will no longer have access to Sklr or any of its services."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                await supabase.auth.signOut();
+                await UserIdStorage.setRememberMe(false);
+                await UserIdStorage.saveLoggedInUserId(-1);
+                if (context.mounted) {
+                  Navigator.of(context, rootNavigator: true).pop();
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const StartPage()));
+                }
+              },
+              child: const Text('Sign Out',
+                  style: TextStyle(color: Color(0xFF6296FF))),
             ),
           ],
-        ),
-      ),
-      bottomNavigationBar: CustomBottomNavigationBar(
-        currentIndex: 3,
-      ),
+        );
+      },
     );
   }
 }
@@ -277,14 +304,12 @@ class _ProfilePageState extends State<ProfilePage> {
 class OptionTile extends StatelessWidget {
   final IconData icon;
   final String title;
-  final Widget? trailing;
-  final Function? onTap;
+  final VoidCallback? onTap;
 
   const OptionTile({
     super.key,
     required this.icon,
     required this.title,
-    this.trailing,
     this.onTap,
   });
 
@@ -293,37 +318,8 @@ class OptionTile extends StatelessWidget {
     return ListTile(
       leading: Icon(icon, color: Colors.grey),
       title: Text(title),
-      trailing: trailing,
-      onTap: () => onTap?.call(),
+      trailing: const Icon(Icons.chevron_right),
+      onTap: onTap,
     );
   }
-}
-
-showSignOutDialog(BuildContext context) {
-  Widget button = TextButton(
-    child: const Text('Sign Out', style: TextStyle(color: Color(0xFF6296FF))),
-    onPressed: () async {
-      Navigator.of(context, rootNavigator: true).pop();
-      await UserIdStorage.setRememberMe(false);
-      await UserIdStorage.saveLoggedInUserId(-1);
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const StartPage()));
-    },
-  );
-
-  AlertDialog dialog = AlertDialog(
-    title: const Text("Confirm signout?"),
-    content: Text(
-        "If you sign out, you will no longer have access to Sklr or any of its services."),
-    actions: [
-      button,
-    ],
-  );
-
-  showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (BuildContext context) {
-        return dialog;
-      });
 }
